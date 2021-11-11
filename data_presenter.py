f3271435dc78c54223fd4959512ba62d4daf6d06
Chris Kim <ckjr92@gmail.com>
2021-11-11 16:38:33 -0700

    finished graph

--- data_presenter.py
+++ data_presenter.py
@@ -1,3 +1,6 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
 #1 and #2
 df = open("CupcakeInvoices.csv")
 
@@ -13,8 +16,45 @@ df = open("CupcakeInvoices.csv")
 #6
 # print(round(sum([float(line.strip().split(',')[3]) * float(line.strip().split(',')[4]) for line in df]),2))
 
+# Part 3
+chocolate = []
+vanilla = []
+strawberry = []
+
+for line in df:
+    line = line.strip().split(',')
+    if line[2] == "Chocolate":
+        chocolate.append(float(line[3]) * float(line[4]))
+    elif line[2] == "Vanilla":
+        vanilla.append(float(line[3]) * float(line[4]))
+    elif line[2] == "Strawberry":
+        strawberry.append(float(line[3]) * float(line[4]))
+
+sum_chocolate = sum(chocolate)
+sum_vanilla = sum(vanilla)
+sum_strawberry = round(sum(strawberry),2)
+
+## code for graph ##
+fig = plt.figure()
+ax = fig.add_axes([0,0,1,1])
+flavors = ['Chocolate', 'Vanilla', 'Strawberry']
+totals = [sum_chocolate, sum_vanilla, sum_strawberry]
+ax.bar(flavors,totals, color =(0.5,0.1,0.5,0.6))
+
+afont = {'fontname':'arial black'}
+bfont = {'fontname':'krungthep'}
+
+plt.title('Cupcake Totals Per Type', fontsize=24, **afont)
+plt.xlabel('Types', fontsize=16, **afont)
+plt.ylabel('Invoice Totals', fontsize=16, **afont)
+
+
+# plt.show()
+
+
 #7
 df.close()
+
 
 
 # import pandas as pd
